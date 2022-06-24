408d3f39a1aa35df16fbb6bd84a3bd4f263b23e9
13-NRGK <79078508+13-NRGK@users.noreply.github.com>
2022-06-25 00:51:41 +0200

    axis’s update

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,11 +1,13 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = -0.15; //andere kant op
+var rotationSpeed = 3; //andere kant op
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-myOtherBox.object3D.rotation.x += rotationSpeed;
+myOtherBox.object3D.rotation.x += rotationSpeed; //draait op de x-as
+myOtherBox.object3D.rotation.y += rotationSpeed; //draait nu ook op de y-as
+myOtherBox.object3D.rotation.z += rotationSpeed; //drait nu ook op de z-as
 console.log(myOtherBox.object3D.rotation.x);
 }
 
