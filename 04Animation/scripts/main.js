28f346aff194a3aad6cc324609f814ae4874e5d1
13-NRGK <79078508+13-NRGK@users.noreply.github.com>
2022-06-26 17:53:13 +0200

    Change speed

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.0004; 
+var rotationSpeed = 1.4; 
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
