63f81328dd817747a66cd56e950aaa047bbc1ec6
13-NRGK <79078508+13-NRGK@users.noreply.github.com>
2022-06-26 21:32:57 +0200

    Update

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,14 +1,19 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 1.4; 
+var rotationSpeed = 0.4; 
+var positionStep= 0.004; //variable for the rate the bar will move
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-myOtherBox.object3D.rotation.x += rotationSpeed; //draait op de x-as
-myOtherBox.object3D.rotation.y += rotationSpeed; //draait nu ook op de y-as
-myOtherBox.object3D.rotation.z += rotationSpeed; //drait nu ook op de z-as
+myOtherBox.object3D.rotation.x += rotationSpeed; //spin on the x-as
+myOtherBox.object3D.rotation.y += rotationSpeed; //spin on the y-as
+myOtherBox.object3D.rotation.z += rotationSpeed; //spin on the z-as
+myOtherBox.object3D.position.x += positionStep;
+myOtherBox.object3D.position.y += positionStep;
+myOtherBox.object3D.position.z += positionStep;
 console.log(myOtherBox.object3D.rotation.x);
+console.log(myOtherBox.object3D.position.x);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
